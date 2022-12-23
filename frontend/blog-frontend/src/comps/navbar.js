3e5814779e97a752775b4eb95ec8a09ef29b4614
mayersab <mayersz01@outlook.com>
2022-12-23 00:01:05 -0500

    added register/sign up buttond

--- frontend/blog-frontend/src/comps/navbar.js
+++ frontend/blog-frontend/src/comps/navbar.js
@@ -3,8 +3,21 @@ import navbar from '../styles/Nav.module.css'
 
 const Navbar = () => {
     return (
-        <div >
-            <nav className={navbar.navbar}>Logo</nav>
+        <div>
+            <nav className={navbar.navbar}>
+                <div className={navbar.logoandsearch}>
+                    <div className={navbar.logo}>Logo</div>
+                    <div className={navbar.searchbar}>
+                        <input placeholder='Search'></input>
+                    </div>
+                </div>
+
+                <div className={navbar.registerandsignin}>
+                    <div className={navbar.register}>Register</div>
+                    <div className={navbar.signin}>Sign in</div>
+                </div>
+            </nav>
+            
         </div>
     );
 }
