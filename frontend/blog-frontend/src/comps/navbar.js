dda8e330db16c6d96b064b1972c3dfb7c88008bc
mayersab <mayersz01@outlook.com>
2022-12-23 13:12:37 -0500

    nav components completed

--- frontend/blog-frontend/src/comps/navbar.js
+++ frontend/blog-frontend/src/comps/navbar.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useState } from 'react';
 import navbar from '../styles/Nav.module.css'
 
 const Navbar = () => {
-    return (
+    const [signin, setsignin] = useState(false);
+    if (signin) {
+        return (
         <div>
             <nav className={navbar.navbar}>
                 <div className={navbar.logoandsearch}>
@@ -12,14 +15,34 @@ const Navbar = () => {
                     </div>
                 </div>
 
+                <div className={navbar.createandgreeting}>
+                    <div className={navbar.createpost}>Create Post</div>
+                    <div className={navbar.greeting}>Hello, User</div>
+                </div>
+            </nav>
+        </div>
+        )
+    } else {
+        return (
+            <div>
+                <nav className={navbar.navbar}>
+                
+                    <div className={navbar.logoandsearch}>
+                        <div className={navbar.logo}>Logo</div>
+                        <div className={navbar.searchbar}>
+                            <input placeholder='Search'></input>
+                        </div>
+                    </div>
+
                 <div className={navbar.registerandsignin}>
                     <div className={navbar.register}>Register</div>
                     <div className={navbar.signin}>Sign in</div>
-                </div>
+                </div> 
+
             </nav>
-            
         </div>
-    );
+        )
+    }
 }
 
 export default Navbar;
